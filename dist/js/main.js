fd54a524377cd49807e6bb7d5f3d44342fc43c43
ftinoco <ftinoco@ia.com.mx>
2016-09-28 19:23:59 -0500

    cambios

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,21 +4,22 @@
   // Trigger mobile menu
 
   // Cache variables
-  var trigger = document.getElementById('menu-trigger'),
+  var body = document.querySelector('body'),
+      trigger = document.getElementById('menu-trigger'),
       menu = document.getElementById('menu-mobile'),
       overlay = document.getElementById('overlay'),
-      items = [].slice.call(document.querySelectorAll('.o-menu--mobile .o-menu__item'));
+      items = document.querySelectorAll('.o-menu--mobile .o-menu__item');
 
   // Bind Events
   overlay.addEventListener('click', function () {
     menuAction();
   });
-  trigger.addEventListener('click', function () {
+  trigger.addEventListener('click', function (e) {
+    e.preventDefault();
     menuAction();
   });
 
   items.forEach(function (item, index) {
-    console.log(item);
     item.addEventListener('click', function () {
       menuAction();
     });
@@ -28,6 +29,7 @@
   function menuAction () {
     toggleClass(menu, 'js-active');
     toggleClass(overlay, 'js-active');
+    toggleClass(body, 'js-noscroll');
   }
 
   function toggleClass (el, className) {
@@ -37,5 +39,47 @@
       el.classList.add(className);
     }
   }
+
+
+  // Gallery functionality
+
+  function openGallery () {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+      {
+        src: 'http://lorempixel.com/output/people-q-c-1024-768-3.jpg',
+        w: 1200,
+        h: 900
+      },
+      {
+        src: 'http://lorempixel.com/output/people-q-c-1024-768-6.jpg',
+        w: 1200,
+        h: 900,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+      }
+    ];
+
+    // define options
+    var options = {
+      index: 0,
+      history: false,
+      shareEl: false,
+      closeOnScroll: false
+    };
+
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  }
+
+  var galleryGridItems = document.querySelectorAll('.o-grid--gallery .o-grid__item');
+
+  galleryGridItems.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+      openGallery();
+    });
+  });
 
 }());
